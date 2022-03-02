f8017d239688b33f6455725af3c27c199fec7f07
Riyad hasan <riyad.hasan@smdevops.com>
2022-03-02 19:16:50 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import { useCallback, useEffect, useState } from 'react';
+import { useDropzone } from 'react-dropzone';
+import './App.css';
+import ClassComponent from './ClassComponent'
+function App() {
+  const [images, setImages] = useState([])
+  function handleUpload() {
+    console.log("Uploading Filess......")
+  }
+  const onDrop = useCallback((acceptedFiles, rejectedFiles) => {
+    console.log("acceptedFiles", acceptedFiles)
+    acceptedFiles.forEach(file => {
+      const reader = new FileReader()
+      reader.onload = () => {
+        setImages(prevState => [...prevState, reader.result])
+      }
+      reader.readAsDataURL(file)
+    })
+    console.log("rejectedFiles", rejectedFiles)
+
+  }, [])
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept: 'image/jpeg,image/png'
+  });
+
+  useEffect(() => {
+    console.log(images)
+  }, [images])
+  console.log(getInputProps(), getRootProps())
+  return (
+    <>
+    <ClassComponent></ClassComponent>
+      <div>
+        <div className='dropzone' {...getRootProps()}>
+          <input {...getInputProps} />
+          {isDragActive ? "drageActive" : "You can drope your file"}
+        </div>
+        {images.length > 0 && <div>
+          {images.map((image, index) => <img className='selected-images' src={image} alt="" />)}
+        </div>}
+
+
+        {images.length > 0 && <button onClick={handleUpload}>Upload </button>}
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/ClassComponent.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+export default class ClassComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            image: null
+        };
+        this.onImageChange = this.onImageChange.bind(this);
+    }
+    onImageChange = event => {
+        if (event.target.files && event.target.files[0]) {
+            let img = event.target.files[0];
+            this.setState({
+                image: URL.createObjectURL(img)
+            });
+        }
+    };
+
+    componentDidMount() { }
+
+    render() {
+
+        return (
+            <div>
+                <div>
+                    <div>
+                        <div style={{display: 'flex', justifyContent: 'center'}}>
+                            <img style={{ height: "100px", width: "100px" }} src={this.state.image} />
+                        </div>
+                        <h1>Select Image</h1>
+                        <input type="file" name="myImage" onChange={this.onImageChange} />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
